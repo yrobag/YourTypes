090f9b1c95bf23b514f31dee004187cea51d83b9
Jakub <jakub@student.edu>
2017-04-09 23:06:46 +0200

    Editing default UserBundle Forms

--- web/js/app.js
+++ web/js/app.js
@@ -1,13 +1,39 @@
 
+// $(document).ready(function () {
+//     $('form :input').blur(function () {
+//         if ($(this).hasClass('myInput1') && $(this).val()) {
+//             if($(this).next('.myInput2').val()){
+//                 $(this).closest('form').submit();
+//             }
+//         }else if ($(this).hasClass('myInput2') && $(this).val()) {
+//             if($(this).prev('.myInput1').val()){
+//                 $(this).closest('form').submit();
+//             }
+//         }
+//     });
+// });
+
 $(document).ready(function () {
     $('form :input').blur(function () {
         if ($(this).hasClass('myInput1') && $(this).val()) {
             if($(this).next('.myInput2').val()){
-                $(this).closest('form').submit();
+                $.ajax({
+                    type: "POST",
+                    url: "/setType",
+                    data: $(this).parent('form').serialize(),
+                    success: function(){
+                    }
+                });
             }
         }else if ($(this).hasClass('myInput2') && $(this).val()) {
             if($(this).prev('.myInput1').val()){
-                $(this).closest('form').submit();
+                $.ajax({
+                    type: "POST",
+                    url: "/setType",
+                    data: $(this).parent('form').serialize(),
+                    success: function(){
+                    }
+                });
             }
         }
     });
